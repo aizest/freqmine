6ffe0e244d55e2c0b87a236cf435edbdf165f227
aigeek <mail2xz@hotmail.com>
2014-04-29 01:53:46 -0400

    Add a new head file, which define the number of pthreads

---
+++ Freqmine/pthread_constants.h
@@ -0,0 +1,14 @@
+/*
+ * pthread_constants.h
+ *
+ *  Created on: Apr 29, 2014
+ *      Author: aigeek
+ */
+
+#ifndef PTHREAD_CONSTANTS_H_
+#define PTHREAD_CONSTANTS_H_
+
+const int WORKING_PTHREADS = 4;
+
+
+#endif /* PTHREAD_CONSTANTS_H_ */
